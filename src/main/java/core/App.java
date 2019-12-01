73792bd51d185d6bc6d4f670fe3926cfc5de217e
AV3N <ilyapech01@gmail.com>
2019-12-01 07:07:35 +0400

    Added card

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -6,6 +6,6 @@ package core;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
 	}
 }
---
+++ src/main/java/core/entities/dickings/Card.java
@@ -0,0 +1,30 @@
+package core.entities.dickings;
+
+import core.entities.*;
+
+public abstract class Card extends Entity {
+
+	protected String cardName;
+	private TypeOfCard cardType;
+	
+	public Card()
+	{
+		this.cardName = "unknow";
+		this.cardType = TypeOfCard.BUILD;
+	}
+
+	public Card(String cName, TypeOfCard cType)
+	{
+		this.cardName = cName;
+		this.cardType = cType;
+	}
+
+	public void setCardName(String cName)
+	{
+		this.cardName = cName;
+	}
+
+	public String getCardName()
+	{
+		return this.cardName;
+	}
